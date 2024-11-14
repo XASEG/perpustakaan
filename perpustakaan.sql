2099e46d8172e601a3ad4a640d635657c5a32672
XASEG <tawmorgen@gmail.com>
2024-11-14 08:42:04 +0700

    Add files via upload

--- perpustakaan.sql
+++ perpustakaan.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 14, 2024 at 02:34 AM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 7.4.24
+-- Generation Time: Nov 14, 2024 at 02:39 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,47 +28,51 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `buku` (
-  `BukuID` int(11) NOT NULL,
-  `Judul` varchar(255) NOT NULL,
-  `Penulis` varchar(255) NOT NULL,
-  `Penerbit` varchar(255) NOT NULL,
-  `TahunTerbit` int(11) NOT NULL
+  `nama_buku` varchar(255) DEFAULT NULL,
+  `penerbit` varchar(255) DEFAULT NULL,
+  `pengarang` varchar(255) DEFAULT NULL,
+  `tahun_terbit` date DEFAULT NULL,
+  `tempat_terbit` varchar(255) DEFAULT NULL,
+  `id_buku` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `buku`
+--
+
+INSERT INTO `buku` (`nama_buku`, `penerbit`, `pengarang`, `tahun_terbit`, `tempat_terbit`, `id_buku`) VALUES
+('alamak', 'cihuy', 'hihi', '2024-10-09', 'batam', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `username` varchar(255) NOT NULL,
+  `password` int(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kategoribuku`
+-- Table structure for table `peminjam`
 --
 
-CREATE TABLE `kategoribuku` (
-  `KategoriID` int(11) NOT NULL,
-  `NamaKategori` varchar(255) NOT NULL
+CREATE TABLE `peminjam` (
+  `id_peminjam` int(25) NOT NULL,
+  `nama_peminjam` varchar(50) NOT NULL,
+  `buku_dpinjam` varchar(50) NOT NULL,
+  `tanggal_peminjam` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `kategoribuku_relasi`
+-- Dumping data for table `peminjam`
 --
 
-CREATE TABLE `kategoribuku_relasi` (
-  `KategoriBukuID` int(11) NOT NULL,
-  `BukuID` int(11) NOT NULL,
-  `KategoriID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `koleksipribadi`
---
-
-CREATE TABLE `koleksipribadi` (
-  `KoleksiID` int(11) NOT NULL,
-  `UserID` int(11) NOT NULL,
-  `BukuID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `peminjam` (`id_peminjam`, `nama_peminjam`, `buku_dpinjam`, `tanggal_peminjam`) VALUES
+(1, 'cihuy', 'alamak', '2024-11-01');
 
 -- --------------------------------------------------------
 
@@ -77,42 +81,174 @@ CREATE TABLE `koleksipribadi` (
 --
 
 CREATE TABLE `peminjaman` (
-  `peminjamanID` int(11) NOT NULL,
-  `UserID` int(11) NOT NULL,
-  `BukuID` int(11) NOT NULL,
-  `TanggalPeminjaman` date NOT NULL,
-  `TanggalPengembalian` date NOT NULL,
-  `StatusPeminjaman` varchar(50) NOT NULL
+  `id_peminjaman` int(11) NOT NULL,
+  `nama_peminjaman` varchar(255) DEFAULT NULL,
+  `tanggal_peminjaman` date DEFAULT NULL,
+  `nohp_peminjaman` int(11) DEFAULT NULL,
+  `nama_buku` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `peminjaman`
+--
+
+INSERT INTO `peminjaman` (`id_peminjaman`, `nama_peminjaman`, `tanggal_peminjaman`, `nohp_peminjaman`, `nama_buku`) VALUES
+(10, 'hahahaa', '2024-10-05', 2147483647, 'mantapjiwa'),
+(15, 'yahaha', '2024-11-02', 24141, 'hahaha');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengembalian`
+--
+
+CREATE TABLE `pengembalian` (
+  `id_pengembalian` int(11) NOT NULL,
+  `nama_pengembalian` varchar(255) NOT NULL,
+  `tanggal_pengembalian` date NOT NULL,
+  `nohp_pengembalian` int(11) NOT NULL,
+  `nama_buku` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pengembalian`
+--
+
+INSERT INTO `pengembalian` (`id_pengembalian`, `nama_pengembalian`, `tanggal_pengembalian`, `nohp_pengembalian`, `nama_buku`) VALUES
+(1, 'hihuha', '2024-10-23', 6970, 'sigma_book');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `perpus`
+--
+
+CREATE TABLE `perpus` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password` int(11) DEFAULT NULL,
+  `tanggal_bergabung` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `perpus`
+--
+
+INSERT INTO `perpus` (`id_user`, `username`, `password`, `tanggal_bergabung`) VALUES
+(5, 'haahaha', 2147483647, '2024-11-10'),
+(6, 'hahaha', 23213131, '2024-11-12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `petugas`
+--
+
+CREATE TABLE `petugas` (
+  `id_petugas` int(20) NOT NULL,
+  `nama_petugas` varchar(25) NOT NULL,
+  `password` int(20) NOT NULL,
+  `jadwal_petugas` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ulasanbuku`
+-- Table structure for table `transaksi`
 --
 
-CREATE TABLE `ulasanbuku` (
-  `UlasanID` int(11) NOT NULL,
-  `UserID` int(11) NOT NULL,
-  `BukuID` int(11) NOT NULL,
-  `Ulasan` text NOT NULL,
-  `Rating` int(11) NOT NULL
+CREATE TABLE `transaksi` (
+  `pengembalian` int(25) NOT NULL,
+  `pembayaran` int(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `user`
+-- Dumping data for table `transaksi`
 --
 
-CREATE TABLE `user` (
-  `UserID` int(11) NOT NULL,
-  `Username` varchar(255) NOT NULL,
-  `Password` varchar(255) NOT NULL,
-  `Email` varchar(255) NOT NULL,
-  `NamaLengkap` varchar(255) NOT NULL,
-  `Alamat` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `transaksi` (`pengembalian`, `pembayaran`) VALUES
+(12, 10),
+(12, 10);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`) USING BTREE;
+
+--
+-- Indexes for table `peminjam`
+--
+ALTER TABLE `peminjam`
+  ADD PRIMARY KEY (`id_peminjam`);
+
+--
+-- Indexes for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id_peminjaman`);
+
+--
+-- Indexes for table `pengembalian`
+--
+ALTER TABLE `pengembalian`
+  ADD PRIMARY KEY (`id_pengembalian`);
+
+--
+-- Indexes for table `perpus`
+--
+ALTER TABLE `perpus`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Indexes for table `petugas`
+--
+ALTER TABLE `petugas`
+  ADD PRIMARY KEY (`id_petugas`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `peminjam`
+--
+ALTER TABLE `peminjam`
+  MODIFY `id_peminjam` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  MODIFY `id_peminjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `pengembalian`
+--
+ALTER TABLE `pengembalian`
+  MODIFY `id_pengembalian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `perpus`
+--
+ALTER TABLE `perpus`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `petugas`
+--
+ALTER TABLE `petugas`
+  MODIFY `id_petugas` int(20) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
